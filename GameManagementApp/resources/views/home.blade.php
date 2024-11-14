cdb018697dbd8a7df6becc6c5155ddbbdaacec6d
BaBence04 <BaBence04@gmail.com>
2024-11-14 13:55:42 +0100

    added css

--- GameManagementApp/resources/views/home.blade.php
+++ GameManagementApp/resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layout')
 
 @section('content')
-<h1>Üdvözölve légy a to the GameManagementApp admin felületén!</h1>
+<h1>Üdvözölve légy a GameManagementApp admin felületén!</h1>
 <p>Használd a navigációt a táblák eléréséhez.</p>
 @endsection
