29d34626a0f1056ac93e1676dea99b2bcf08d500
3041TAN-12 <3041TAN-12@tanuzem.boronkay.local>
2025-01-28 13:56:19 +0100

    helyesen nem keresi már a home viewt, 404et ad

--- Freya-REST/routes/web.php
+++ Freya-REST/routes/web.php
@@ -1,10 +1,1 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PlayerController;
-use App\Http\Controllers\GameController;
-use App\Http\Controllers\PlayerGameController;
-
-Route::get('/', function () {
-    return view('home');
-})->name('home');
