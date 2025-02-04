d057329542c167286a85f5d52fac8e9e3d3354bc
Érsek Huba <ersekhuba@gmail.com>
2025-02-04 13:05:05 +0100

    adjusted roots, added examples of secured endpoints

--- Freya-REST/routes/api.php
+++ Freya-REST/routes/api.php
@@ -6,6 +6,29 @@ use App\Http\Controllers\PlantController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\UserPlantController;
 
+//demonstrate how to be able to set diffrent autorizatoin levels
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/admin/example', function (Request $request) {
+        // Only users with 'admin' ability can access this route
+    })->middleware('ability:admin');
+
+    Route::get('/stats/example', function (Request $request) {
+        // Only users with 'stats' ability can access this route
+    })->middleware('ability:stats,user');
+
+    Route::get('/user/example', function (Request $request) {
+        // Only users with 'viewer' ability can access this route
+    })->middleware('ability:user');
+
+    //más format, lehet jogosultság alapján groupolni
+    Route::middleware('ability:user')->group(function () {
+        Route::get('users', [UserController::class, 'index']);
+        Route::resource('plants', PlantController::class);
+    });
+
+});
+
+
 Route::post('user/login', [UserController::class,'login']);
 Route::get('user', function (Request $request) {
     return $request->user();
@@ -13,5 +36,5 @@ Route::get('user', function (Request $request) {
 
 
 Route::resource('plants', PlantController::class);
-Route::resource('users', UserController::class);
+//Route::resource('users', UserController::class);
 Route::resource('userplants', UserPlantController::class);
