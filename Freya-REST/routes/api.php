92537fc33e80f08e9d051727c2a42a8640b07407
Érsek Huba <ersekhuba@gmail.com>
2025-02-13 13:42:57 +0100

    regruped requests

--- Freya-REST/routes/api.php
+++ Freya-REST/routes/api.php
@@ -1,13 +1,13 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Sanctum\PersonalAccessToken;
 use App\Http\Controllers\PlantController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\UserPlantController;
-
-
+use App\Http\Middleware\Abilities;
 
 //login
 Route::post('user/login', [UserController::class,'login']);
@@ -15,7 +15,18 @@ Route::post('user/login', [UserController::class,'login']);
 Route::resource('plants', PlantController::class);
 Route::resource('userplants', UserPlantController::class);
 
-//returns all uers
+//requires users abilities
 Route::middleware(['auth:sanctum', 'abilities:user'])->group(function () {
+    Route::get('/listings',[ListingController::class, 'index']);
+    Route::get('/listings/{id}',[ListingController::class, 'show']);
+});
+
+//requires stats abilities
+Route::middleware(['auth:sanctum', 'abilties:stats'])->group(function () {
+
+});
+
+//requires admin abilities
+Route::middleware(['auth:sanctum', 'abilities:admin'])->group(function (){
     Route::get('/users', [UserController::class, 'index']);
 });
