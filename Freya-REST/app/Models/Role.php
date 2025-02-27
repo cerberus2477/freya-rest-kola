4e2339a60fb276d5bb619b27d833ace6792441ee
3041TAN-13 <3041TAN-13@tanuzem.boronkay.local>
2025-02-27 13:53:57 +0100

    added category migration and model
    todo: seeder and requests

--- Freya-REST/app/Models/Role.php
+++ Freya-REST/app/Models/Role.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    protected $fillable = ['role_name'];
+    protected $fillable = ['name'];
     public $timestamps=false;
 
     public function user()
--- Freya-REST/database/migrations/2025_02_27_124935_create_categories_table.php
+++ Freya-REST/database/migrations/2025_02_27_124935_create_categories_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('stages', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
         });
@@ -22,7 +22,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user_plants');
-        Schema::dropIfExists('stages');
+        Schema::dropIfExists('articles');
+        Schema::dropIfExists('categories');
     }
 };
