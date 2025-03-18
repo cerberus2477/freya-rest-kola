e327f37e349ea448104bd22c386d3ac2d6762c46
cerberus2477 <tabor.tunde@gmail.com>
2025-03-18 22:14:20 +0100

    as the title says' -m 'a listingseederben fontos az az egy sor, így nem hal be a seeder

---
+++ Freya-REST/app/Http/Requests/TypeRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TypeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return $this->storeRules();
+    }
+
+    /**
+     * Get the validation rules that apply to the store request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    protected function storeRules(): array
+    {
+        return [
+            'name' => 'required|string|unique:types,name|max:255|min:3',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'A típusnév megadása kötelező.',
+            'name.unique' => 'A megadott típus már létezik.',
+            'name.max' => 'A típus maximum 255 karakter hosszú lehet.',
+            'name.min' => 'A típus minimum 3 karakter hosszú kell legyen.',
+        ];
+    }
+}
